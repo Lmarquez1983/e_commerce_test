58fee1f09507278ed6eff9b3863e85e62f79b716
Luis Marquez <lmarquez1@ncstudents.niagaracollege.ca>
2023-02-28 12:24:02 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import { Products, Navbar } from "./components";
 import { commerce } from "./library/commerce";
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
 
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, qty) => {
+    const item = await commerce.cart.add(productId, qty);
+    setCart(item.cart);
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
+  console.log(cart);
 
   return (
     <div>
       <Navbar />
-      <Products products={products} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
